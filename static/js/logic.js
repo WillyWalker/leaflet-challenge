1fe1cd6979e8a1e9b9883be0457fa55baddb9785
William Walker <williamwalker@Williams-MacBook-Air.local>
2023-11-13 23:28:53 -0800

    code and webpage

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,56 +1,82 @@
 
+
 // Store our API endpoint as queryUrl.
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-var Data = {"type":"Feature",
-"properties":{"mag":1.46,"place":"1 km ESE of Talmage, CA","time":1699032114610,"updated":1699042570384,
-"tz":null,"url":"https://earthquake.usgs.gov/earthquakes/eventpage/nc73955590",
-"detail":"https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/nc73955590.geojson",
-"felt":null,"cdi":null,"mmi":null,"alert":null,"status":"automatic","tsunami":0,"sig":33,
-"net":"nc","code":"73955590","ids":",nc73955590,","sources":",nc,",
-"types":",nearby-cities,origin,phase-data,scitech-link,","nst":10,"dmin":0.05885,"rms":0.07,
-"gap":145,"magType":"md","type":"earthquake","title":"M 1.5 - 1 km ESE of Talmage, CA"},
-"geometry":{"type":"Point","coordinates":[-123.1521683,39.1290016,7.07]},"id":"nc73955590"}
-
-// coordinates = [feature.geometry.coordinates[0], feature.geometry.coordinates[1]]
-
-// depth = feature.geometry.coordinates[2]
-
 // Perform a GET request to the query URL
-d3.json(queryUrl).then(function (Data) {
+d3.json(queryUrl).then(function (data) {
   // Once we get a response, send the data.features object to the createFeatures function.
-  
-  init(Data);
+  createFeatures(data);
 });
 
+  // Once we get a response, send the data.features object to the createFeatures function.
 let myMap = L.map("map", {
-  center: [39, -122],
-  zoom: 8
+  center: [38, -123],
+  zoom: 7
 });
 
-function init() {
-  data = [{
-    x: -123.1521683,
-    y: 39.1290016,
-    //x: [0, 500, 1000, 1500, 2000, 2500, 3000],
-    //y: [0, 500, 1000, 1500, 2000, 2500, 3000],
+// Add a tile layer.
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
 
-    text: "1 km ESE of Talmage, CA",
-    mode: 'markers',
-    marker: {'autocolorscale': false, 'colorscale': 'YlOrRd', 'color': 'Green'},
-    type: 'scatter',
-    name: 'Exoplanets',
-
-    //text: planetNames,
-    customdata: 7.07,
-    hovertemplate:
-    '<b> %{text} </b><br>' +
-    'Coordinates: (%{x}, %{y})<br>' +
-    'Earth-Like Score: %{marker.color} out of 100<br>' +
-    'Focus Depth: %{customdata} km' +
-    '<extra></extra>'
-    
-}];
-
+// Define a markerSize() function that will give each city a different radius based on its population.
+function markerSize(magnitude) {
+  return magnitude * 5000;
 }
+// Loop through the cities array, and create one marker for each city object.
+function createFeatures(data) {
 
+  // const length = data.type["metadata"]["count"];
+  const length = data.metadata.count;
+
+  for (let i = 1; i <= length; i++) {
+
+    var magnitude = data.features[i]["properties"]["mag"]
+    var place = data.features[i]["properties"]["place"]
+    var Focus = data.features[i]["geometry"]["coordinates"][2]
+    var time = data.features[i]["properties"]["time"]
+
+// Color Function
+    Rfunc = 10 + 30 * Math.sqrt(Focus)
+    Gfunc = 1 + 3 * Math.sqrt(Focus)
+    Bfunc = 2 + 6 * Math.sqrt(Focus)
+
+// Set limits to color values
+    if (Focus < 0) {
+      RVal = `00`;
+    } else if (Rfunc > 207) {
+      RVal = `cf`;
+    } else if (Rfunc < 15.5) {
+      RVal = `0${Math.round(Rfunc).toString(16)}`;
+    } else {RVal = Math.round(Rfunc).toString(16);}
+
+    if (Focus < 0 ) {
+      GVal = `00`;
+    }  else if (Gfunc > 16) {
+      GVal = `10`;
+    } else if (Gfunc < 15.5) {
+      GVal = `0${Math.round(Gfunc).toString(16)}`;
+    } else {GVal = Math.round(Gfunc).toString(16);}
+
+    if (Focus < 0) {
+      BVal = `00`;
+    } else if (Bfunc > 32) {
+      BVal = `20`;
+    } else if (Bfunc < 15.5) {
+      BVal = `0${Math.round(Bfunc).toString(16)}`;
+    } else {BVal = Math.round(Bfunc).toString(16);}
+
+    //var colorType = `#84427`
+    var colorType = `#${RVal}${GVal}${BVal}`
+
+    L.circle( [data.features[i]["geometry"]["coordinates"][1], data.features[i]["geometry"]["coordinates"][0]], {
+      fillOpacity: 0.75,
+      color: colorType,
+      radius: markerSize(magnitude)
+    }).bindPopup(`<h3>${place}</h3> <hr> 
+    <h3> Time: ${new Date(time)}</h3> 
+    <h3> Magnitude: ${magnitude} </h3>
+    <h3> Focus: ${Focus} km below surface</h3>`).addTo(myMap);
+  }
+}
